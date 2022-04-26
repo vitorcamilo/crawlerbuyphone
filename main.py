8775bed4118b6743eb6600a241c30146fd848bf8
VItor Camilo <vitor.camilo@frenet.com.br>
2022-04-26 18:52:26 -0300

    google sku crawl

--- main.py
+++ main.py
@@ -21,14 +21,10 @@ class webcrawl():
         self.Opptions.binary_location = os.environ.get('FIREFOX_BIN')
         self.Opptions.add_argument("--disable-dev-shm-usage")
         self.Opptions.add_argument("--no-sandbox")
-        self.Opptions.set_preference("general.useragent.override", "New user agent")
         self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options = self.Opptions)
 
   
     def iniciarcrawler(self):
-        self.driver.get("https://www.google.com/search?q=whats+my+ip")
-        ip = self.driver.find_element(By.XPATH,value="/html/body")
-        print(ip.text)
         sendtelegram = 'Lá vamos nós novamente.... Crawler iniciado!'
         self.telegramresponse(sendtelegram)
         iphones = self.apirequest()
@@ -36,97 +32,60 @@ class webcrawl():
         if dados :
             for dado in dados:
                 try:
-                    nameiphone = dado['name']
-                    memoryiphone = dado['memory']
-                    idiphone = dado['id']
-                    coriphone = dado['color']
+                    #nameiphone = dado['name']
+                    #memoryiphone = dado['memory']
+                    #coriphone = dado['color']
                     # skumagalu = dado['magalu_sku']
-                    casasbahiasku = dado['casasbahia_sku']
-                    americanassku = dado['americanas_sku']
-                    ptofriosku = dado['ponto_sku']
+                    # casasbahiasku = dado['casasbahia_sku']
+                    # americanassku = dado['americanas_sku']
+                    # ptofriosku = dado['ponto_sku']
+                    idiphone = dado['id']
+                    googleSKU = dado['google_sku']
+                    
 
-                    if casasbahiasku == None:
-                        vaziocb = True
+                    if googleSKU == None:
+                        precoamericanas = 0
+                        precocasasbahia = 0
+                        precomagalu = 0
+                        precopontofrio = 0
                     else:
-                        vaziocb = False
-                    if americanassku == None:
-                        vazioamericanas = True
-                    else:
-                        vazioamericanas = False
-                    if ptofriosku == None:
-                        vazioptofrio = True
-                    else:
-                        vazioptofrio= False
-
-                    # precomagalu = self.magalu(skumagalu, coriphone)
-                    precoamericanas = self.americanas(americanassku, vazioamericanas)
-                    time.sleep(random.randint(0, 30))
-                    precocasasbahia = self.casasbahia(casasbahiasku, vaziocb)
-                    time.sleep(random.randint(0, 30))
-                    precopontofrio = self.pontofrio(ptofriosku, vazioptofrio)
-                    time.sleep(random.randint(0, 30))
+                        self.driver.get(f'''https://www.google.com/shopping/product/{googleSKU}/''')
+                        precomagalu = '0'
+                        precoamericanas = '0'
+                        precocasasbahia = '0'
+                        precopontofrio = '0'
+                        lojas = self.driver.find_elements(By.XPATH, value= "(//th[text()='Vendido por']/following::a[@class='b5ycib shntl'])")
+                        precos = self.driver.find_elements(By.XPATH, value="//a[@class='sh-osd__sort']/following::div[@class='drzWO']")
+                        for l, p in zip(lojas, precos):
+                            if l.text == "Magazine Luiza" :
+                                precomagalu = p.text.replace("R$","").replace(",","").replace(".","").replace(" ","")
+                            elif l.text == "Pontofrio.com":
+                                precopontofrio = p.text.replace("R$","").replace(",","").replace(".","").replace(" ","")
+                            elif l.text == "Americanas.com":
+                                precoamericanas = p.text.replace("R$","").replace(",","").replace(".","").replace(" ","")
+                            elif l.text == "Casas Bahia":
+                                precocasasbahia = p.text.replace("R$","").replace(",","").replace(".","").replace(" ","")
+                            
+                            print(f'''magalu:{precomagalu}{os.linesep}pontofrio:{precopontofrio}{os.linesep}Americanas:{precoamericanas}{os.linesep}Casas bahia:{precocasasbahia}''')
+                    
                     urlamericanas = f'''https://pedidos.buyphone.com.br/api/products/{idiphone}/americanas/{precoamericanas}'''
                     urlcasasbahia = f'''https://pedidos.buyphone.com.br/api/products/{idiphone}/casasbahia/{precocasasbahia}'''
                     urlpontofrio = f'''https://pedidos.buyphone.com.br/api/products/{idiphone}/ponto/{precopontofrio}'''
+                    urlmagalu = f'''https://pedidos.buyphone.com.br/api/products/{idiphone}/magalu/{precomagalu}'''
                     self.pricereturn(urlamericanas)
                     self.pricereturn(urlcasasbahia)
                     self.pricereturn(urlpontofrio)
-                    self.driver.quit()
+                    self.pricereturn(urlmagalu)
+                    
+                    if idiphone == 87:
+                        sendtelegram = 'Pheeew.... O crawler terminou por hoje!.'
+                        self.telegramresponse(sendtelegram)
 
 
                 except Exception as e:
                     print(e)
                     sendtelegram = 'O Crawler encontrou um problema! Hora de olhar o que está acontecendo....'
                     self.telegramresponse(sendtelegram)
-
-
-
-
-    # def magalu(self, SKU, color):
-    #     self.driver.get(f'''https://www.magazineluiza.com.br/busca/{SKU}''')
-    #     precosmagalu = self.driver.find_elements(By.XPATH, value ="(//p[contains(@class,'sc-hKwDye dWfgMa')])[1]")
-    #     for iphone in precosmagalu:
-    #         if color in iphone.text:
-    #             return iphone.text
-
-    def americanas(self, SKU, vazio):
-        if vazio == True:
-            americanasfinal=0
-        else:
-            self.driver.get(f'''https://www.americanas.com.br/busca/{SKU}''')
-            americanas0=self.driver.find_element(By.XPATH, value="//span[contains(@class,'src__Text-sc-154pg0p-0 price__PromotionalPrice-sc-h6xgft-1')]")
-            americanas1 = americanas0.text
-            americanas2 = americanas1.replace("R$","")
-            americanas3 = americanas2.replace(",","")
-            americanasfinal = americanas3.replace(".","")
-        #print(magalufinalprice)
-        return americanasfinal
-
-    def casasbahia(self, SKU, vazio):
-        if vazio == True:
-            cbfinalprice = 0
-        else:
-            self.driver.get(f'''https://www.casasbahia.com.br/{SKU}/b''')
-            cb0 = self.driver.find_element(By.XPATH, value="//span[@class='ProductPrice__PriceValue-sc-1tzw2we-6 kBYiGY']")
-            cb1 = cb0.text
-            cb2 = cb1.replace("R$","")
-            cb3 = cb2.replace(",","")
-            cbfinalprice = cb3.replace(".","")
-            #print(cbfinalprice)
-        return cbfinalprice
-
-    def pontofrio(self, SKU, vazio):
-        if vazio == True:
-            PTfriofinalprice = 0
-        else:
-            self.driver.get(f'''https://www.pontofrio.com.br/{SKU}/b''')
-            PTfrio0 = self.driver.find_element(By.XPATH, value="//span[@class='ProductPrice__PriceValue-sc-1tzw2we-6 kBYiGY']")
-            PTfrio1 = PTfrio0.text
-            PTfrio2 = PTfrio1.replace("R$","")
-            PTfrio3 = PTfrio2.replace(",","")
-            PTfriofinalprice = PTfrio3.replace(".","")
-            #print(PTfriofinalprice)
-        return PTfriofinalprice
 
 
     def apirequest(self):
@@ -142,19 +101,15 @@ class webcrawl():
         'token': 'ef7223f0-55b4-49a7-9eed-f4b4ef14b2f1'
         }
         requests.post(url, headers=headers)
-
+        
     def telegramresponse(self, text):
         url = f'''https://api.telegram.org/bot5098238913:AAHvT080O9ifLyIdB5ICE_MoE16nsAcEoNE/sendMessage?chat_id=-574442548&text={text}'''
         requests.get(url)
     
-    
-crawl = webcrawl()
-crawl.iniciarcrawler()
 
-
-# if __name__ == "__main__":
-#     crawl= webcrawl()
-#     schedule.every().day.at("03:00").do(crawl.iniciarcrawler)
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
+if __name__ == "__main__":
+    crawl= webcrawl()
+    schedule.every().day.at("03:00").do(crawl.iniciarcrawler)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
